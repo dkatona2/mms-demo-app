537dec3243a59d39a06b31fbe1dbade4b6a9e284
dkatona2 <katonapontdavid@gmail.com>
2022-09-21 19:53:52 +0200

    test test

---
+++ src/test/java/com/tsystems/mms/demoapp/user/UserServiceTestWithDB.java
@@ -0,0 +1,34 @@
+package com.tsystems.mms.demoapp.user;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootTest
+public class UserServiceTestWithDB {
+
+    private UserService userService;
+    private PasswordEncoder passwordEncoder;
+    private User user;
+
+    @MockBean
+    private UserRepository userRepositoryMock;
+
+    @BeforeEach
+    public void setUp() {
+        userService = new UserService(userRepositoryMock, passwordEncoder);
+        user = new User();
+        user.setId(1L);
+        user.setEmail("katonapontdavid@gmail.com");
+        user.setPassword("asd123");
+    }
+
+    @Test
+    void testGetAll() {
+        // Mock
+//        Mockito.when(userRepositoryMock.findAll().
+    }
+}
