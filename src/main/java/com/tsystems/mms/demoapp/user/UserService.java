11e28fde5b86a6ffe8ed1152dcb037d2190a89ed
dkatona2 <katonapontdavid@gmail.com>
2022-09-22 08:37:24 +0200

    Task 1

--- src/main/java/com/tsystems/mms/demoapp/user/UserService.java
+++ src/main/java/com/tsystems/mms/demoapp/user/UserService.java
@@ -30,15 +30,30 @@ public class UserService {
         this.passwordEncoder = passwordEncoder;
     }
 
+    /**
+     *
+     * Returns the user by id.
+     *
+     */
     public User getUserById(Long id) {
         return userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("User not found with id: " + id));
     }
 
+    /**
+     *
+     * Mapping the entity to the DTO.
+     *
+     */
     private void mapUserData(User originalUser, UserDTO userDTO) {
         BeanUtils.copyProperties(userDTO, originalUser);
 
     }
 
+    /**
+     * Delete a user from the database.
+     *
+     * @return a boolean whether the deleting was successful or not.
+     */
     public boolean deleteUser(Long id) {
         boolean success = false;
         User user = getUserById(id);
@@ -51,6 +66,11 @@ public class UserService {
         return success;
     }
 
+    /**
+     * Update an already saved user.
+     *
+     * @return a boolean whether the save was successful or not.
+     */
     public boolean updateUser(Long id, UserDTO userDTO) {
         boolean success = false;
         User user = getUserById(id);
@@ -64,7 +84,12 @@ public class UserService {
         return success;
     }
 
-
+    /**
+     *
+     *
+     * Save user with an appropiate DTO
+     * @return the saved user's id.
+     */
     public Long saveUser(UserDTO userDTO) {
         User newUser = new User(userDTO);
         newUser.setPassword(passwordEncoder.encode(userDTO.getPassword()));
